7c41778652aa77ba44eff4d14dc19b0bfff13729
Emmanuel Mouton <mou@foyer.lu>
2017-09-04 11:28:59 +0200

    Creating a sample node Hapi application

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Good = require('good');
+
+//Launch the web server and make it listen to a given port
+const server = new Hapi.Server();
+server.connection({port: 3000, host: 'localhost'});
+
+//Sample routes
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+//Serving static html pages
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: function (request, reply) {
+            reply.file('./public/hello.html');
+        }
+    });
+});
+
+//Setting up logging with Good
+server.register({
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                    response: '*',
+                    log: '*'
+                }]
+            }, {
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+}, (err) => {
+
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
